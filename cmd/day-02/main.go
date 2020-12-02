3a8dc737476ab1d21647c6e0dd68b8614dc9739d
james.gawn <james@gawn.uk>
2020-12-02 11:32:33 +0000

    Completed day 2

--- cmd/day-02/main.go
+++ cmd/day-02/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type passwordForVerification struct {
@@ -13,6 +15,11 @@ type passwordForVerification struct {
 	password             string
 }
 
+type passwordResult struct {
+	passwordForVerification
+	valid bool
+}
+
 func main() {
 	arr := make([]passwordForVerification, 0)
 	scanner := bufio.NewScanner(os.Stdin)
@@ -20,13 +27,51 @@ func main() {
 		scanner.Scan()
 		text := scanner.Text()
 		if len(text) != 0 {
-			minChar, _ := strconv.Atoi(text[0:1])
-			maxChar, _ := strconv.Atoi(text[2:3])
-			requiredCharacter := text[4:5]
-			password := text[7:]
+			endOfMin := strings.Index(text, "-")
+			startOfMax := endOfMin + 1
+			endOfMax := strings.Index(text, " ")
+			startOfChar := endOfMax + 1
+			endOfChar := endOfMax + 2
+			startOfPassword := endOfChar + 2
+			minChar, _ := strconv.Atoi(text[0:endOfMin])
+			maxChar, _ := strconv.Atoi(text[startOfMax:endOfMax])
+			requiredCharacter := text[startOfChar:endOfChar]
+			password := text[startOfPassword:]
 			arr = append(arr, passwordForVerification{minChar, maxChar, requiredCharacter, password})
 		} else {
 			break
 		}
 	}
+	part1ValidCount := 0
+	part2ValidCount := 0
+	for _, toVerify := range arr {
+		resultPart1 := verifyPasswordValidityPart1(toVerify)
+		if resultPart1.valid {
+			part1ValidCount++
+		}
+		resultPart2 := verifyPasswordValidityPart2(toVerify)
+		if resultPart2.valid {
+			part2ValidCount++
+		}
+	}
+	fmt.Println("Total Valid (Part 1): " + strconv.Itoa(part1ValidCount))
+	fmt.Println("Total Valid (Part 2): " + strconv.Itoa(part2ValidCount))
+}
+
+func verifyPasswordValidityPart1(verifiable passwordForVerification) passwordResult {
+	characterCount := 0
+	requiredRune := verifiable.requiredCharacter
+	for _, character := range verifiable.password {
+		if string(character) == requiredRune {
+			characterCount++
+		}
+	}
+	return passwordResult{verifiable, characterCount >= verifiable.minCharacterRequired && characterCount <= verifiable.maxCharacterRequired}
+}
+
+func verifyPasswordValidityPart2(verifiable passwordForVerification) passwordResult {
+	requiredRune := verifiable.requiredCharacter
+	firstCharMatch := verifiable.password[verifiable.minCharacterRequired-1:verifiable.minCharacterRequired] == requiredRune
+	secondCharMatch := verifiable.password[verifiable.maxCharacterRequired-1:verifiable.maxCharacterRequired] == requiredRune
+	return passwordResult{verifiable, firstCharMatch != secondCharMatch}
 }
