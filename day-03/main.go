583f86ae6bb60ef941cac382132debffff63fd27
james.gawn <james@gawn.uk>
2020-12-03 17:21:47 +0000

    Simplified folder structure, and added make files to simplify build.

---
+++ day-03/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arr := make([][]rune, 0)
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if len(text) != 0 {
+			arr = append(arr, []rune(text))
+		} else {
+			break
+		}
+	}
+
+	part1 := obtainTrees("part 1", arr, 1, 3)
+	fmt.Println("Part 1: " + strconv.Itoa(part1))
+
+	part21 := obtainTrees("part2.1", arr, 1, 1)
+	part22 := obtainTrees("part2.2", arr, 1, 3)
+	part23 := obtainTrees("part2.3", arr, 1, 5)
+	part24 := obtainTrees("part2.4", arr, 1, 7)
+	part25 := obtainTrees("part2.5", arr, 2, 1)
+	part2 := part21 * part22 * part23 * part24 * part25
+	fmt.Println("Part 2: " + strconv.Itoa(part2))
+
+	cErr := file.Close()
+	if cErr != nil {
+		fmt.Println(cErr)
+	}
+}
+
+func obtainTrees(name string, course [][]rune, downSpeed int, rightSpeed int) int {
+	height := len(course)
+	width := len(course[0])
+	numberOfTrees := 0
+	i := 0
+	for posY := 0; posY < height; posY = posY + downSpeed {
+		rowArray := course[posY]
+		posX := obtainXPosition(i, width, rightSpeed)
+		value := string(rowArray[posX : posX+1])
+		treePresent := value == "#"
+		if treePresent {
+			numberOfTrees++
+		}
+		fmt.Println("Course: " + name + " Y:" + strconv.Itoa(posY) + " X:" + strconv.Itoa(posX) + " V: " + value + " TreePresent: " + strconv.FormatBool(treePresent) + " TreeCount:" + strconv.Itoa(numberOfTrees))
+		i++
+	}
+	fmt.Println("Course " + name + " tree count:" + strconv.Itoa(numberOfTrees))
+	return numberOfTrees
+}
+
+func obtainXPosition(i int, width int, rightwardSpeed int) int {
+	a := rightwardSpeed * i
+	b := a % (width)
+	if a < width {
+		return a
+	} else {
+		return b
+	}
+}
